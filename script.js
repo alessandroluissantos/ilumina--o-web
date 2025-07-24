5263d7a933f7db4ad1ed33540000f00d2ead2ce0
alessandroluissantos <118467387+alessandroluissantos@users.noreply.github.com>
2025-07-24 13:56:16 -0300

    rev6

--- script.js
+++ script.js
@@ -3,15 +3,24 @@ let lampadaCozinhaLigada = false;
 
 function ligarLampadaSala() {
     if (lampadaSalaLigada === false) {
-        document.getElementById("lamp-sala").classList.add("acesa");
+        document.getElementById("lamp-sala").classList.add("acesa");    
+        document.getElementById("bt-sala").textContent = "Desligar"
         lampadaSalaLigada = true;
 
     } else {
-        document.getElementById("lamp-sala").classList.remove("acesa");
+        document.getElementById("lamp-sala").classList.remove("acesa")
+        document.getElementById("bt-sala").textContent = "Ligar"
         lampadaSalaLigada = false;
     }
-
-function ligarLampadaCozinha() {
-    alert("ligarLampadaCozinha");
-
 }
+function ligarLampadaCozinha() {
+    if (lampadaCozinhaLigada == false) {
+        lampadaCozinhaLigada = true;
+        document.getElementById("bt-cozinha").textContent = "Desligar"
+    } else {
+        lampadaCozinhaLigada = false;
+        document.getElementById("bt-cozinha").textContent = "Ligar"
+    }
+    document.getElementById("lamp-cozinha").classList.toggle("acesa")
+}
+
