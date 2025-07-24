b933a7160480d8538abef19dfadbf3d9f68e38a9
alessandroluissantos <118467387+alessandroluissantos@users.noreply.github.com>
2025-07-24 11:13:54 -0300

    rev5

--- script.js
+++ script.js
@@ -1,10 +1,15 @@
-
+let lampadaSalaLigada = false;
+let lampadaCozinhaLigada = false;
 
 function ligarLampadaSala() {
-    alert("ligarLampadaSala");
+    if (lampadaSalaLigada === false) {
+        document.getElementById("lamp-sala").classList.add("acesa");
+        lampadaSalaLigada = true;
 
-
-}
+    } else {
+        document.getElementById("lamp-sala").classList.remove("acesa");
+        lampadaSalaLigada = false;
+    }
 
 function ligarLampadaCozinha() {
     alert("ligarLampadaCozinha");
