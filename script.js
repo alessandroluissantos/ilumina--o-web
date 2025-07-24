bdbbb029e34e3c5a920219e7997638f8a23a54cf
alessandroluissantos <118467387+alessandroluissantos@users.noreply.github.com>
2025-07-24 14:19:16 -0300

    rev7

--- script.js
+++ script.js
@@ -1,6 +1,22 @@
 let lampadaSalaLigada = false;
 let lampadaCozinhaLigada = false;
 
+let clienteWeb = null;
+
+const clientId = 'Esp32' + Math.floor(Math.random() * 900) + 100;
+clienteWeb = new Paho.MQTT.Client('broker.hivemq.com', 8884, clientId);
+
+clienteWeb.connect({   
+    useSSL: true, 
+    onSuccess: function() {
+        alert('A conexão com Broker foi bem sucedida')
+    },
+    onFailure: function() {
+        alert('A conexão com Broker falhou')
+    }
+});
+
+
 function ligarLampadaSala() {
     if (lampadaSalaLigada === false) {
         document.getElementById("lamp-sala").classList.add("acesa");    
